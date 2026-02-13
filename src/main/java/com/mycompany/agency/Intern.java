575244ee53378b23ac5fca763133d7f7c7e8605c
Fransisco <crazyashtro@gmail.com>
2026-02-12 21:09:08 -0500

    Implemented Intern Class

--- src/main/java/com/mycompany/agency/Intern.java
+++ src/main/java/com/mycompany/agency/Intern.java
@@ -4,7 +4,7 @@ package com.mycompany.agency;
 
     // TODO 1: Make Intern a child of StaffEmploee
 
-public class Intern 
+public class Intern extends StaffEmployee
 {
     //-----------------------------------------------------------------
     //  TODO 2: Constructor: Sets up this intern using the specified
@@ -12,14 +12,15 @@ public class Intern
     //-----------------------------------------------------------------
     public Intern(String eName, String eAddress, String ePhone)
     {
-        
+        super(eName, eAddress, ePhone, "", 0);
     }
 
     //-----------------------------------------------------------------
     // TODO 3:  Returns a zero pay value for this intern.
     //-----------------------------------------------------------------
+    @Override
     public double pay()
     {
-      
+      return 0.0;
     }
 }
